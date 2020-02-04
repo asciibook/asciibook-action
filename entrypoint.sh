9cbbe4c45e3b90eaf5558bf96357915ce7449c66
Rei <chloerei@gmail.com>
2020-02-04 20:33:41 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 env
-REPO_NAME="$(echo $GITHUB_REPOSITORY | cut -d'/' -f2)"
-docker run -v $RUNNER_WORKSPACE/$REPO_NAME:/asciibook asciibook/asciibook asciibook build $INPUT_SOURCE --format html
+REPO_NAME="$(echo ${GITHUB_REPOSITORY} | cut -d'/' -f2)"
+docker run -v ${RUNNER_WORKSPACE}/${REPO_NAME}:/asciibook asciibook/asciibook asciibook build ${INPUT_SOURCE} --format html
 
 cd build/html
 touch .nojekyll
@@ -12,6 +12,6 @@ git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 git commit -m 'asciibook build'
-git push --force https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git master:gh-pages
+git push --force https://${GITHUB_ACTOR}${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git master:gh-pages
 rm -fr .git
 cd ../..
