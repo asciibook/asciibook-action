fd123d9f8db4f18a0f39dac957f896fafce2f500
Rei <chloerei@gmail.com>
2020-02-04 20:53:08 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-env
 REPO_NAME="$(echo ${GITHUB_REPOSITORY} | cut -d'/' -f2)"
 docker run -v ${RUNNER_WORKSPACE}/${REPO_NAME}:/asciibook asciibook/asciibook asciibook build ${INPUT_SOURCE} --format html
 
