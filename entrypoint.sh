cc45033d157cc98ed8d099cab86e77e315f2dca6
Rei <chloerei@gmail.com>
2020-02-04 17:40:40 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 env
-docker run -v /home/runner/work/asciibook-action/asciibook-action:/asciibook asciibook/asciibook ls -la
+docker run -v /home/runner/work/asciibook-action/asciibook-action:/asciibook asciibook/asciibook pwd && ls -la
